957f17b61e5d1317964f8f267e3329aa53b92e55
Takuya Mizunaka <is0089he@gmail.com>
2015-04-19 23:18:11 +0900

    第二部の８章まで終了

---
+++ ex0801.rb
@@ -0,0 +1,12 @@
+# -*- coding utf-8 -*-
+# 表示したい蔵書データを作成する
+title = "実践アジャイル ソフトウェア開発法とプロジェクト管理"
+auther = "山田 正樹"
+yomi = "やまだ まさき"
+publisher = "ソフトリサーチセンター"
+
+# 蔵書データを表示する
+puts title
+puts auther
+puts yomi
+puts publisher
